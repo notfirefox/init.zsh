52c2bfa8909a41b3a98ef18ea2010250f9ae4383
notfirefox <115221458+notfirefox@users.noreply.github.com>
2023-02-07 23:29:35 +0100

    Update

--- init.zsh
+++ init.zsh
@@ -20,6 +20,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 ZVM_INIT_MODE='sourcing'
 
 # source zsh plugins
-source "$HOME/.zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
-source "$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$HOME/.config/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
+source "$HOME/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$HOME/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
